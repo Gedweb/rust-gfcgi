d144cc97e48f55fda8c87af5f71f208f986c8646
Gedweb <vert@gedweb.name>
2016-05-04 00:30:42 +0300

    Documentation init

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['gfcgi'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='gfcgi/model/struct.Header.html' title='gfcgi::model::Header'>Header</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='gfcgi/model/struct.Request.html' title='gfcgi::model::Request'>Request</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='gfcgi/model/struct.Response.html' title='gfcgi::model::Response'>Response</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='gfcgi/struct.Stream.html' title='gfcgi::Stream'>Stream</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
