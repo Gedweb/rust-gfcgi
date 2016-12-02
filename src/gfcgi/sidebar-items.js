12e52349d02225b5ba9bbeaa07099556af13f9bb
Gedweb <agridchin@condenast.ru>
2016-12-02 14:40:26 +0300

    v0.4.1

--- src/gfcgi/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["Client",""],["HttpPair","HTTP request / response pairs"],["Request",""],["Response","HTTP implementation of response"],["StreamSyntax","FasctCGI request parser"]],"trait":[["Handler","Callback trait"]]});
